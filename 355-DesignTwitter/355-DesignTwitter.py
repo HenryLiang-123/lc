9cf2b113f57845cc7271258dfbf57ddeaae7bcbe
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-02 19:20:33 -0600

    Update file 2/2/2025, 7:20:33 PM

--- 355-DesignTwitter/355-DesignTwitter.py
+++ 355-DesignTwitter/355-DesignTwitter.py
@@ -1,74 +1,22 @@
-from collections import deque
+from collections import defaultdict, deque
+
 class Twitter:
 
     def __init__(self):
-        self.user_to_tweet = defaultdict(deque)
-        self.user_to_following = defaultdict(set)
-        self.tweet_to_time = {}
-        self.time = 0
+        self.follows = defaultdict(set)
+        self.feed = deque()
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        # post to self
-        self.user_to_tweet[userId].appendleft(tweetId)
-        if len(self.user_to_tweet[userId]) > 10:
-            expired_tweet = self.user_to_tweet[userId].pop()
-            del self.tweet_to_time[expired_tweet]
-
-        # record time and sort
-        self.tweet_to_time[tweetId] = self.time
-        # self.tweet_to_time = {k: self.tweet_to_time[k] for k in sorted(self.tweet_to_time)}
-        self.time += 1
+        self.feed.appendleft((userId, tweetId))
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        news_self = list(self.user_to_tweet[userId])
-        followers = list(self.user_to_following[userId])
-        news_follower = []
-        for i in range(len(followers)):
-            news_of_follower = self.user_to_tweet[followers[i]]
-            news_follower.extend(news_of_follower)
-        
-        n_self = len(news_self)
-        n_follower = len(news_follower)
-        p1, p2 = 0,0
-        result = []
-        news_to_time_self = sorted([(self.tweet_to_time[i], i) for i in news_self], reverse=True)
-        news_to_time_follower = sorted([(self.tweet_to_time[i], i) for i in news_follower], reverse=True)
-        while p1 < n_self and p2 < n_follower:
-            tweetId_self = news_to_time_self[p1][1]
-            tweetId_follower = news_to_time_follower[p2][1]
+        return [tweetId for user, tweetId in self.feed if userId == user or user in self.follows[userId]][:10]
 
-            if news_to_time_self[p1][0] > news_to_time_follower[p2][0]:
-                result.append(tweetId_self)
-                p1 += 1
-            else:
-                result.append(tweetId_follower)
-                p2 += 1
-        
-        if p1 < n_self and len(result) < 10:
-            while len(result) < 10 and p1 < n_self:
-                tweetId_self = news_to_time_self[p1][1]
-                result.append(tweetId_self)
-                p1 += 1
-        else:
-            while len(result) < 10 and p2 < n_follower:
-                tweetId_follower = news_to_time_follower[p2][1]
-                result.append(tweetId_follower)
-                p2 += 1
-
-        # tweetId_self = news_self[i]
-        # tweetId_follower = news_follower[i]
-
-
-        return result[:10]
-        
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.user_to_following[followerId].add(followeeId)
+        self.follows[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId in self.user_to_following[followerId]:
-            self.user_to_following[followerId].remove(followeeId)
-
-        
+        self.follows[followerId].discard(followeeId)
 
 
 # Your Twitter object will be instantiated and called as such:
