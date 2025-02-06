6292b2122fef87e6a49177ebcc2d2fa75733ba66
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-06 13:28:58 -0600

    Create file

---
+++ 1726-TuplewithSameProduct/1726-TuplewithSameProduct.py
@@ -0,0 +1,17 @@
+import math
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        product_count = defaultdict(int)
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                product = nums[i] * nums[j]
+                product_count[product] = product_count.get(product, 0) + 1
+
+        result = 0
+        print(product_count)
+        for key, value in product_count.items():
+            if value >= 2:
+                result += math.perm(value, 2) * 4
+
+        return result
