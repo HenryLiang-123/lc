b75732250050bdc99124008576aebdb1bbddf961
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-11 21:17:03 -0600

    File updated at 2/11/2025, 9:17:03 PM

--- 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.py
+++ 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.py
@@ -1,50 +1,44 @@
 class TrieNode:
     def __init__(self):
-        self.links = {}
-        self.end = False
-    
+        self.children = {}
+        self.isWord = False
+
 class WordDictionary:
 
     def __init__(self):
         self.root = TrieNode()
-        self.maxL = 0
         
-    def addWord(self, word):
-        """
-        :type word: str
-        :rtype: None
-        """
-        node = self.root
-        l = 0
-        for w in word:
-            if w not in node.links:
-                node.links[w] = TrieNode()
-            node = node.links[w]
-            l += 1
-        
-        self.maxL = max(self.maxL, l)
-        node.end = True
 
-    def search(self, word):
-        """
-        :type word: str
-        :rtype: bool
-        """
-        if len(word) > self.maxL:
-            return False
-        
-        def helper(index, node):
-            for inn in range(index, len(word)):
-                c = word[inn]
+    def addWord(self, word: str) -> None:
+        curr_node = self.root
+        for c in word:
+            if c not in curr_node.children:
+                curr_node.children[c] = TrieNode()
+            curr_node = curr_node.children[c]
+        curr_node.isWord = True
+
+    def search(self, word: str) -> bool:
+        # curr_node = self.root
+        n = len(word)
+        def dfs(curr_node, start):
+            for i in range(start, n):
+                c = word[i]
                 if c == ".":
-                    for child in node.links.values():
-                        if helper(inn+1, child):
+                    for nei in curr_node.children.values():
+                        if dfs(nei, i+1):
                             return True
                     return False
                 else:
-                    if c not in node.links:
+                    if c not in curr_node.children:
                         return False
-                    node = node.links[c]
+                    curr_node = curr_node.children[c]
 
-            return node.end
-        return helper(0, self.root)
+            return curr_node.isWord
+
+        return dfs(self.root, 0)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
