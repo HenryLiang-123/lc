87179e7a56baaf248b7312e2826d0af569122f70
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-13 21:41:47 -0600

    File updated at 2/13/2025, 9:41:47 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -1,4 +1,20 @@
- while (first != null) {
-        first = first.next;
-        second = second.next;
-    }
+def removeNthFromEnd(self, head, n):
+
+    p1 = p2 = head
+    for i in range(n):
+        p1 = p1.next
+
+    ## Delete the head node
+    if p1 == None:
+        return head.next
+
+    while(p1.next != None):
+        p1 = p1.next
+        p2 = p2.next
+    ## Delete the node
+    if n == 1: # If delete the last node, set the last node is None
+        p2.next = None
+    else:
+        p2.next = p2.next.next
+
+    return head
