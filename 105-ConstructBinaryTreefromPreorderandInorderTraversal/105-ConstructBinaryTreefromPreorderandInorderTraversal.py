22777f1089abbf62691e806fae84123ce2e36ca2
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-13 23:26:00 -0600

    File updated at 2/13/2025, 11:26:00 PM

--- 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.py
+++ 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.py
@@ -6,14 +6,25 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        num_to_index_inorder = {}
+        for i, num in enumerate(inorder):
+            num_to_index_inorder[num] = i
 
-        if not preorder or not inorder:
-            return None
+        root_index = 0
+        def dfs(left, right):
+            nonlocal root_index
+            if left > right:
+                return None
+            print(root_index)
+            root = TreeNode(preorder[root_index])
+            split_index = num_to_index_inorder[root.val]
+            root_index += 1
 
-        root = TreeNode(preorder[0])
-        left_end = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:left_end+1], inorder[:left_end+1])
-        root.right = self.buildTree(preorder[left_end+1:], inorder[left_end+1:])
+            root.left = dfs(left, split_index - 1)
+            root.right = dfs(split_index + 1, right)
 
-        return root
+            return root
 
+        return dfs(0, len(preorder)-1)
+
+            
