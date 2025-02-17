b1a6566522a1d4501c49519a215c63457c00f8b3
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-16 18:41:28 -0600

    File updated at 2/16/2025, 6:41:27 PM

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
@@ -1,35 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
 
-        result = 0
-        max_left = 0
-        max_right = 0
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        class DiameterData(object):
+            def __init__(self, diameter, height):
+                self.diameter = diameter
+                self.height = height
 
-        def dfs(root, path_len):
-            nonlocal result
+        def calculateDiameterAndHeight(root):
             if not root:
-                return 0
-            
-            left_length = dfs(root.left, path_len+1)
-            right_length = dfs(root.right, path_len+1)
+                return DiameterData(0, 0)
 
-            result = max(left_length+right_length, result)
+            leftData = calculateDiameterAndHeight(root.left)
+            rightData = calculateDiameterAndHeight(root.right)
 
-            return max(left_length, right_length) + 1
+            currentDiameter = max(leftData.height + rightData.height,
+                                  max(leftData.diameter, rightData.diameter))
+            currentHeight = max(leftData.height, rightData.height) + 1
 
-            # print(root.val, left_length, right_length)
+            return DiameterData(currentDiameter, currentHeight)
 
-
-        dfs(root, 0)
-        return result
-
-            
-
-
+        data = calculateDiameterAndHeight(root)
+        return data.diameter
