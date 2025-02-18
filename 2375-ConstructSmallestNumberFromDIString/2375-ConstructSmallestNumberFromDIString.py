1a2415e3ddd8b99fd8038988a6aa6fc8e1f3306f
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-18 12:56:25 -0600

    File updated at 2/18/2025, 12:56:25 PM

--- 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.py
+++ 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.py
@@ -1,44 +1,56 @@
-// Last updated: 2/18/2025, 12:54:18 PM
+// Last updated: 2/18/2025, 12:56:25 PM
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        counts = {str(i):1 for i in range(1, 10)}
-        n = len(pattern) + 1
         result = []
-        def dfs(i, path):
-            if i == n:
-                result.append(path[:])
-                return True
-            
-            if i > n:
-                return False
+        stack = []
+        n = len(pattern)
 
-            for choice, _ in counts.items():
-                if i == 0:
-                    path.append(choice)
-                    counts[choice] -= 1
-                    if dfs(i+1, path):
-                        return True
-                    path.pop()
-                    counts[choice] += 1
-                else:
-                    curr_pattern = pattern[i-1]
-                    if curr_pattern == "I":
-                        if counts[choice] > 0 and path[-1] < choice:
-                            path.append(choice)
-                            counts[choice] -= 1
-                            if dfs(i+1, path):
-                                return True
-                            path.pop()
-                            counts[choice] += 1
-                    else:
-                        if counts[choice] > 0 and path[-1] > choice:
-                            path.append(choice)
-                            counts[choice] -= 1
-                            if dfs(i+1, path):
-                                return True
-                            path.pop()
-                            counts[choice] += 1
-            return False
-        dfs(0, [])
-        return "".join(result[0])
+        for i in range(n + 1):
+            stack.append(i+1)
+
+            if i == n or pattern[i] == "I":
+                while stack:
+                    result.append(str(stack.pop()))
+
+        return "".join(result)
+        # counts = {str(i):1 for i in range(1, 10)}
+        # n = len(pattern) + 1
+        # result = []
+        # def dfs(i, path):
+        #     if i == n:
+        #         result.append(path[:])
+        #         return True
+            
+        #     if i > n:
+        #         return False
+
+        #     for choice, _ in counts.items():
+        #         if i == 0:
+        #             path.append(choice)
+        #             counts[choice] -= 1
+        #             if dfs(i+1, path):
+        #                 return True
+        #             path.pop()
+        #             counts[choice] += 1
+        #         else:
+        #             curr_pattern = pattern[i-1]
+        #             if curr_pattern == "I":
+        #                 if counts[choice] > 0 and path[-1] < choice:
+        #                     path.append(choice)
+        #                     counts[choice] -= 1
+        #                     if dfs(i+1, path):
+        #                         return True
+        #                     path.pop()
+        #                     counts[choice] += 1
+        #             else:
+        #                 if counts[choice] > 0 and path[-1] > choice:
+        #                     path.append(choice)
+        #                     counts[choice] -= 1
+        #                     if dfs(i+1, path):
+        #                         return True
+        #                     path.pop()
+        #                     counts[choice] += 1
+        #     return False
+        # dfs(0, [])
+        # return "".join(result[0])
 
