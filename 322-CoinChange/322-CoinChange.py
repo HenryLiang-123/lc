e051347cbb2af9631321ee83a5958880afa0b0ce
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-18 16:54:48 -0600

    File updated at 2/18/2025, 4:54:48 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,34 +1,32 @@
-// Last updated: 2/17/2025, 2:03:56 PM
+// Last updated: 2/18/2025, 4:54:48 PM
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo = {}
+        
         def dfs(path_sum):
-            
             if path_sum in memo:
-                return memo[path_sum]
+                return memo[(path_sum)]
 
             if path_sum == amount:
-                memo[path_sum] = 0
                 return 0
 
             if path_sum > amount:
-                memo[path_sum] = float('inf')
                 return float('inf')
+            result = float('inf')
+            for coin in coins:
+                path_sum += coin
+                res = dfs(path_sum)
+                if res != float('inf'):
+                    result = min(result, res+1)
+                path_sum -= coin
 
-            min_coins = float('inf')
-            for c in coins:
-                path_sum += c
-                result = dfs(path_sum)
-                path_sum -= c
-                if result != float('inf'):
-                    min_coins = min(min_coins, result + 1)
+            memo[path_sum] = result
 
-            memo[path_sum] = min_coins
-            return memo[path_sum]
-
-        result = dfs(0)
-        
-        if result != float('inf'):
             return result
+
+        r = dfs(0)
+        if r != float('inf'):
+            return r
         else:
             return -1
+
