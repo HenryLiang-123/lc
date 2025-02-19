6860a4dbcb8875499ba00eea4b570a0750700051
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-19 14:23:11 -0600

    File updated at 2/19/2025, 2:23:11 PM

--- 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn/1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn.py
+++ 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn/1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn.py
@@ -1,4 +1,4 @@
-// Last updated: 2/19/2025, 2:22:44 PM
+// Last updated: 2/19/2025, 2:23:11 PM
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
         if k > 3 * (2 ** (n - 1)):
