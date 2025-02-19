bb1c96246be0b296fed3dfe5f11c5f5f139083f2
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-18 22:59:43 -0600

    File updated at 2/18/2025, 10:59:42 PM

--- 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.py
+++ 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.py
@@ -1,3 +1,4 @@
+// Last updated: 2/18/2025, 10:59:42 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,25 +7,24 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        root_to_index = {}
-        for i, val in enumerate(inorder):
-            root_to_index[val] = i
+        root_idx = 0
+        n = len(inorder)
+        node_to_idx = {inorder[i]:i for i in range(n)}
 
-        root_index = 0
         def dfs(left, right):
-            nonlocal root_index
-
+            nonlocal root_idx
             if left > right:
                 return None
 
-            root = TreeNode(preorder[root_index])
-            pivot = root_to_index[root.val]
-
-            root_index += 1
-
-            root.left = dfs(left, pivot - 1)
-            root.right = dfs(pivot + 1, right)
+            root = TreeNode(preorder[root_idx])
+            root_idx += 1
+            root_in_inorder = node_to_idx[root.val]
+            root.left = dfs(left, root_in_inorder -1)
+            root.right = dfs(root_in_inorder + 1, right)
 
             return root
 
-        return dfs(0, len(preorder) - 1)
+        return dfs(0, n-1)
+
+
+            
