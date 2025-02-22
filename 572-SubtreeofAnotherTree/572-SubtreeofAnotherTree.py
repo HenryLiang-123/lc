e554d196a85b9aa280bdd856793b1eedfc483c31
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-22 16:09:46 -0600

    File updated at 2/22/2025, 4:09:46 PM

--- 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.py
+++ 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.py
@@ -1,4 +1,4 @@
-// Last updated: 2/19/2025, 12:47:08 PM
+// Last updated: 2/22/2025, 4:09:46 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,30 +6,28 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def check(self, root1, root2):
-        if root1 is None and root2 is None:
-                return True
-
-        if root1 is None or root2 is None:
-            return False
-
-        if root1.val != root2.val:
-            return False
-        
-        return self.check(root1.left, root2.left) and self.check(root1.right, root2.right)
-        
-
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def dfs(root, subroot):
-            # print(root.val, subroot.val)
-            if root is None or subroot is None:
-                return root is None and subroot is None
-
-            if not self.check(root, subroot):
-                return dfs(root.left, subroot) or dfs(root.right, subroot)
-            else:
+        def check(root1, root2):
+            if root1 is None and root2 is None:
                 return True
-                
 
+            if root1 is None or root2 is None:
+                return False
+
+            return root1.val == root2.val and check(root1.left, root2.left) and check(root1.right, root2.right)
+
+        def dfs(root, subroot):
+            if root is None and subroot is None:
+                return True
+
+            if root is None or subroot is None:
+                return False
+
+            if not check(root, subroot):
+                return dfs(root.left, subroot) or dfs(root.right, subroot)
+
+            return True
 
         return dfs(root, subRoot)
+
+            
