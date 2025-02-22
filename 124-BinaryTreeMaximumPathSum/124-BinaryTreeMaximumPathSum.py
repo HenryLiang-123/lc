82526c720ff8d766de0da93d7d37b7f8bbbcadca
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-22 15:53:14 -0600

    File updated at 2/22/2025, 3:53:14 PM

--- 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
@@ -1,3 +1,26 @@
-left_gain = max(gain_from_left_subtree, 0)
-right_gain = max(gain_from_right_subtree, 0)
-gain_from_subtree = max(left_gain, right_gain) + root.val
+// Last updated: 2/22/2025, 3:53:14 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        result = float('-inf')
+
+        def dfs(root):
+            nonlocal result
+            if not root:
+                return 0
+
+            left_value = max(dfs(root.left), 0)
+            right_value = max(dfs(root.right), 0)
+
+            result = max(result, left_value + right_value + root.val)
+
+            return max(left_value, right_value) + root.val
+
+        dfs(root)
+        
+        return result
