c3c08e6e4eb8770a7ac4867ba2db4a9941436db4
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-21 18:05:53 -0600

    Create file

---
+++ 90-SubsetsII/90-SubsetsII.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        # nums_set = set(nums)
+        n = len(nums)
+        result = []
+        def dfs(path, start):
+            result.append(path[:])
+
+
+            for i in range(start, n):
+                if i != start and nums[i] == nums[i-1]:
+                    continue
+                # if nums[i] not in path:
+                path.append(nums[i])
+                dfs(path, i + 1)
+                path.pop()
+
+
+        dfs([], 0)
+        return result
