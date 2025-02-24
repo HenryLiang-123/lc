647b450abb56fe7306449e63f5ffe5fdf3249e4f
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-24 17:21:26 -0600

    Create file

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -0,0 +1,15 @@
+def longestCommonPrefix(self, strs):
+
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
+
+    answer = ""
+
+    for i in zip(*strs):
+        if len(set(i)) == 1:
+            answer += i[0]
+        else:
+            break
+    return answer
