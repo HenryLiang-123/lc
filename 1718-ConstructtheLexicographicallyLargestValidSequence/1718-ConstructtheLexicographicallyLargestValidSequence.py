971903d1ce93840aa265f207206bb626a0266f28
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-24 20:27:10 -0600

    File updated at 2/24/2025, 8:27:09 PM

--- 1718-ConstructtheLexicographicallyLargestValidSequence/1718-ConstructtheLexicographicallyLargestValidSequence.py
+++ 1718-ConstructtheLexicographicallyLargestValidSequence/1718-ConstructtheLexicographicallyLargestValidSequence.py
@@ -1,29 +1,27 @@
+// Last updated: 2/24/2025, 8:27:09 PM
 class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
-        seen = set()
+        slots = 2 * (n-1) + 1
+        result = [0 for _ in range(slots)]
         choices = [i for i in reversed(range(1, n+1))]
-        result = [0 for _ in range((n-1)*2 +1)]
-
+        print(choices)
+        seen = set()
         def dfs(idx):
-            # nonlocal result
-            if idx == len(result):
-                # we've filled it out
+            if idx == slots:
                 return True
 
-            for c in reversed(range(1, n+1)):
-                # if we've used the current choice or if the space is occupied, we try next
+            for c in choices:
                 if c in seen:
                     continue
+                # print(idx + c)
                 if c > 1 and (idx + c >= len(result) or result[idx+c] > 0):
                     continue
-                
-                # use curr element
+
                 seen.add(c)
                 result[idx] = c
                 if c > 1:
-                    result[idx+c] = c
+                    result[idx + c] = c
 
-                # next spot
                 next_spot = idx + 1
                 while next_spot < len(result) and result[next_spot]:
                     next_spot += 1
@@ -34,18 +32,17 @@ class Solution:
                 seen.remove(c)
                 result[idx] = 0
                 if c > 1:
-                    result[idx+c] = 0
-
+                    result[idx + c] = 0
+            
             return False
 
         dfs(0)
+        return (result)
+                
+
+                
 
 
-            
 
-        return result
-            
 
-            
 
-            
