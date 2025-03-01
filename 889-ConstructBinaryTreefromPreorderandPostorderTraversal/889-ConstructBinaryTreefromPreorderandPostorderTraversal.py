4072c2bb28da55f84b43378c876ec6c04c24338e
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-28 20:25:45 -0600

    File updated at 2/28/2025, 8:25:45 PM

--- 889-ConstructBinaryTreefromPreorderandPostorderTraversal/889-ConstructBinaryTreefromPreorderandPostorderTraversal.py
+++ 889-ConstructBinaryTreefromPreorderandPostorderTraversal/889-ConstructBinaryTreefromPreorderandPostorderTraversal.py
@@ -1,3 +1,4 @@
+// Last updated: 2/28/2025, 8:25:45 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,26 +7,22 @@
 #         self.right = right
 class Solution:
     def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        n = len(preorder)
-        post_node_to_idx = {postorder[i]:i for i in range(n)}
-        idx = 0
-        def dfs(left_pre, right_pre, left_post, right_post):
-            if left_pre > right_pre or left_post > right_post:
+        n = len(postorder)
+        mapping = {postorder[i]:i for i in range(n)}
+        def dfs(pre_i, pre_j, post_i, post_j):
+            
+            if pre_i > pre_j or post_i > post_j:
+                print(pre_i, pre_j, post_i, post_j)
                 return None
-
-            root = TreeNode(preorder[left_pre])
-            if left_pre != right_pre:
-                left_node = preorder[left_pre + 1]
-                left_size = post_node_to_idx[left_node] - left_post + 1
-
-                root.left = dfs(left_pre + 1, left_pre + left_size, left_post, post_node_to_idx[left_node])
-                root.right = dfs(left_pre + left_size + 1, right_pre, post_node_to_idx[left_node] + 1, right_post - 1)
+            # print(pre_i)
+            root = TreeNode(preorder[pre_i])
+            if pre_i != pre_j:
+                next_node = preorder[pre_i + 1]
+                node_in_post = mapping[next_node]
+                length = node_in_post - post_i + 1
+                root.left = dfs(pre_i+1, pre_i + length, post_i, node_in_post)
+                root.right = dfs(pre_i + length + 1, pre_j, node_in_post+1, post_j-1)
 
             return root
 
-        return dfs(0, n-1, 0, n-1)
-
-
-            
-
-        
+        return dfs(0,n-1, 0, n-1)
