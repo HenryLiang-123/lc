7ac25a96e6689df5e5e02fcff1ef76931d5e774d
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-01 15:05:58 -0600

    Create file

---
+++ 146-LRUCache/146-LRUCache.py
@@ -0,0 +1,58 @@
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        # remove a node
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def add(self, node):
+        # add to beginning
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.add(self.cache[key])
+            return self.cache[key].val
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        node = ListNode(key, value)
+        self.cache[key] = node
+        self.add(node)
+
+        if len(self.cache) > self.capacity:
+            # remove from end
+            last_node = self.tail.prev
+            self.remove(last_node)
+            del self.cache[last_node.key]
+
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
