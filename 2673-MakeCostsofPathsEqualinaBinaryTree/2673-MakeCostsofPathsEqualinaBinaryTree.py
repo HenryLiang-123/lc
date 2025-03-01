3b1e8b4ac34b7e343436140be692cf7053f89490
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-02-28 19:09:15 -0600

    File updated at 2/28/2025, 7:09:14 PM

--- 2673-MakeCostsofPathsEqualinaBinaryTree/2673-MakeCostsofPathsEqualinaBinaryTree.py
+++ 2673-MakeCostsofPathsEqualinaBinaryTree/2673-MakeCostsofPathsEqualinaBinaryTree.py
@@ -1,7 +1,19 @@
-// Last updated: 2/28/2025, 2:51:49 PM
-    for(int i=1;i<N/2;i++)
-    {
-        ans+= Math.abs(cost[i*2+1]-cost[i*2+2]);
-    }
-    return ans;
-}
+// Last updated: 2/28/2025, 7:09:14 PM
+class Solution:
+    def minIncrements(self, n: int, cost: List[int]) -> int:
+        result = 0
+        def dfs(node):
+            nonlocal result
+
+            if node >= n:
+                return 0
+
+            left_val = dfs(2*(node+1)-1)
+            right_val = dfs(2*(node+1))
+
+            result += abs(left_val - right_val)
+
+            return max(left_val, right_val) + cost[node]
+
+        dfs(0)
+        return result
