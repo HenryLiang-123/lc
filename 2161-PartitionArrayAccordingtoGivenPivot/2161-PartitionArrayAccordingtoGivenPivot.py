8328db8f56b2433a68b61038023173791fb07cdb
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-03 13:15:48 -0600

    File updated at 3/3/2025, 1:15:48 PM

--- 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.py
+++ 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.py
@@ -1,15 +1,41 @@
+// Last updated: 3/3/2025, 1:15:48 PM
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        less = []
-        more = []
-        pivots = []
         n = len(nums)
-        for i in range(n):
+        i, j = 0, n-1
+        i2, j2 = 0, n-1
+        res = [0 for _ in range(n)]
+        while i < n:
             if nums[i] < pivot:
-                less.append(nums[i])
-            elif nums[i] == pivot:
-                pivots.append(nums[i])
-            elif nums[i] > pivot:
-                more.append(nums[i])
+                res[i2] = nums[i]
+                i2 += 1
+            if nums[j] > pivot:
+                res[j2] = nums[j]
+                j2 -= 1
 
-        return less + pivots + more
+            i += 1
+            j -= 1
+
+        while i2 <= j2:
+            res[i2] = pivot
+            res[j2] = pivot
+            i2 += 1
+            j2 -= 1
+
+        return res
+
+                
+
+        # less = []
+        # more = []
+        # pivots = []
+        # n = len(nums)
+        # for i in range(n):
+        #     if nums[i] < pivot:
+        #         less.append(nums[i])
+        #     elif nums[i] == pivot:
+        #         pivots.append(nums[i])
+        #     elif nums[i] > pivot:
+        #         more.append(nums[i])
+
+        # return less + pivots + more
