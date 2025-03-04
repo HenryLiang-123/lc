2f18bc557848c8526f1f70c6707a487aefb55ce7
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-04 13:32:30 -0600

    File updated at 3/4/2025, 1:32:30 PM

--- 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.py
+++ 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.py
@@ -1,3 +1,4 @@
+// Last updated: 3/4/2025, 1:32:30 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,26 +7,21 @@
 #         self.right = right
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        result = []
-        i = 0
+        
         def dfs(left, right):
-            res = []
             if left > right:
-                res.append(None)
-                return res
+                return [None]
 
+            res = []
             for i in range(left, right + 1):
-                
-                left_tree = dfs(left, i-1)
-                right_tree = dfs(i+1, right)
-                for l in left_tree:
-                    for r in right_tree:
+                left_trees = dfs(left, i-1)
+                right_trees = dfs(i+1, right)
+                for l in left_trees:
+                    for r in right_trees:
                         root = TreeNode(i+1)
                         root.left = l
                         root.right = r
                         res.append(root)
-
             return res
 
-        return (dfs(0, n-1))
-        # return result
+        return dfs(0, n-1)
