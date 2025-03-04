2769e12bed0f370286e4530e36f54e39dc7c0980
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-04 13:09:45 -0600

    File updated at 3/4/2025, 1:09:45 PM

--- 146-LRUCache/146-LRUCache.py
+++ 146-LRUCache/146-LRUCache.py
@@ -1,5 +1,6 @@
+// Last updated: 3/4/2025, 1:09:45 PM
 class ListNode:
-    def __init__(self, key, val):
+    def __init__(self, key=None, val=None):
         self.key = key
         self.val = val
         self.next = None
@@ -9,45 +10,44 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
         self.head = ListNode(-1, -1)
         self.tail = ListNode(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
-
-    def remove(self, node):
-        # remove a node
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
+        self.key_to_node = {}
 
     def add(self, node):
-        # add to beginning
         node.next = self.head.next
         node.prev = self.head
         self.head.next.prev = node
         self.head.next = node
 
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.add(self.cache[key])
-            return self.cache[key].val
+        if key in self.key_to_node:
+            node = self.key_to_node[key]
+            self.remove(node) # remove from DLL
+            self.add(node) # Add to beginning
+            return node.val
         else:
             return -1
-
+        
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        node = ListNode(key, value)
-        self.cache[key] = node
-        self.add(node)
+        
+        if key in self.key_to_node:
+            node = self.key_to_node[key]
+            self.remove(node)
 
-        if len(self.cache) > self.capacity:
-            # remove from end
+        new_node = ListNode(key, value)
+        self.add(new_node)
+        self.key_to_node[key] = new_node
+        if len(self.key_to_node) > self.capacity:
             last_node = self.tail.prev
             self.remove(last_node)
-            del self.cache[last_node.key]
+            del self.key_to_node[last_node.key]
 
         
 
