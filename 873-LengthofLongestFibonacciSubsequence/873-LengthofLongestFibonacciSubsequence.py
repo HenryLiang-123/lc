82484b45b7c5dbd06e47da2fcd0bceb73f8a1635
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-04 12:22:21 -0600

    File updated at 3/4/2025, 12:22:20 PM

--- 873-LengthofLongestFibonacciSubsequence/873-LengthofLongestFibonacciSubsequence.py
+++ 873-LengthofLongestFibonacciSubsequence/873-LengthofLongestFibonacciSubsequence.py
@@ -1,33 +1,21 @@
+// Last updated: 3/4/2025, 12:22:20 PM
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
-        result = 0
         n = len(arr)
-        s = set(arr)
-        # dp = [[0 for _ in range(n)] for _ in range(n)]
-        for i in range(n-1):
-            for j in range(i+1, n):
-                f1, f2 = arr[i], arr[j]
-                curr = 0
-                while f1 + f2 in s:
-                    curr += 1
-                    f1, f2 = f2, f1+f2
-                result = max(result, curr)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        result = 0
+        for i in range(2, n):
+            start, end = 0, i-1
+            target = arr[i]
+            while start < end:
+                if arr[start] + arr[end] == target:
+                    dp[end][i] = dp[start][end] + 1
+                    result = max(result, dp[end][i])
+                    start += 1
+                    end -= 1
+                elif arr[start] + arr[end] < target:
+                    start += 1
+                elif arr[start] + arr[end] > target:
+                    end -= 1
 
         return result + 2 if result else 0
-
-        # for i in range(2, n):
-        #     target = arr[i]
-        #     left, right = 0, i-1
-        #     while left < right:
-        #         if arr[left] + arr[right] == target:
-        #             dp[right][i] = dp[left][right] + 1
-        #             result = max(result, dp[right][i])
-        #             right -= 1
-        #             left += 1
-        #         elif arr[left] + arr[right] > target:
-        #             right -= 1
-        #         elif arr[left] + arr[right] < target:
-        #             left += 1
-
-        # return result + 2 if result else 0
-                
