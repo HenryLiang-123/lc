98e6aa247b51ca5bcb5fad396a3f411af2a7a4e9
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-07 17:08:09 -0600

    File updated at 3/7/2025, 5:08:09 PM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,4 +1,4 @@
-// Last updated: 3/7/2025, 5:03:10 PM
+// Last updated: 3/7/2025, 5:08:09 PM
 """
 # Definition for a Node.
 class Node:
@@ -12,7 +12,6 @@ from collections import deque
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         old_to_new = {}
-        old_to_new2 = {}
         def bfs(root):
             if not root:
                 return root 
@@ -25,12 +24,13 @@ class Solution:
                         new_node = Node(nei.val, [])
                         old_to_new[nei] = new_node
                         q.append(nei)
-                    else:
+                    # else:
+                    old_to_new[old_node].neighbors.append(old_to_new[nei])
                         # print(old_node.val)
-                        if old_to_new[old_node] not in old_to_new[nei].neighbors:
-                            old_to_new[nei].neighbors.append(old_to_new[old_node])
-                        if old_to_new[nei] not in old_to_new[old_node].neighbors:
-                            old_to_new[old_node].neighbors.append(old_to_new[nei])
+                        # if old_to_new[old_node] not in old_to_new[nei].neighbors:
+                        #     old_to_new[nei].neighbors.append(old_to_new[old_node])
+                        # if old_to_new[nei] not in old_to_new[old_node].neighbors:
+                        #     old_to_new[old_node].neighbors.append(old_to_new[nei])
 
             return old_to_new[root]
 
