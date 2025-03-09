10dfa858b0117408a60a55fa3a0912bf56b10f5e
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-09 14:18:15 -0500

    File updated at 3/9/2025, 2:18:14 PM

--- 3208-AlternatingGroupsII/3208-AlternatingGroupsII.py
+++ 3208-AlternatingGroupsII/3208-AlternatingGroupsII.py
@@ -1,7 +1,7 @@
+// Last updated: 3/9/2025, 2:18:14 PM
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
         arr = colors + colors[:k-1]
-        print(arr)
         n = len(arr)
         left = 0
         result = 0
