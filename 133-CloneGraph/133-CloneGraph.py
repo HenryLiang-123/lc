5352bddb3e6f65390e1d1ad05e3ee9cb8832a441
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-09 22:38:32 -0500

    File updated at 3/9/2025, 10:38:32 PM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,4 +1,4 @@
-// Last updated: 3/7/2025, 5:14:37 PM
+// Last updated: 3/9/2025, 10:38:32 PM
 """
 # Definition for a Node.
 class Node:
@@ -8,45 +8,23 @@ class Node:
 """
 
 from typing import Optional
-from collections import deque
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        seen = {}
+        old_to_new = {}
+
         def dfs(root):
             if not root:
                 return root
 
-            if root in seen:
-                return seen[root]
+            if root in old_to_new:
+                return old_to_new[root]
 
             new_node = Node(root.val, [])
-            seen[root] = new_node
+            old_to_new[root] = new_node
             for nei in root.neighbors:
                 new_node.neighbors.append(dfs(nei))
-                
-            
+
             return new_node
 
         return dfs(node)
-        # def bfs(root):
-        #     if not root:
-        #         return root 
-        #     old_to_new[root] = Node(root.val, [])
-        #     q = deque([root]) # old node
-        #     while q:
-        #         old_node = q.popleft()
-        #         for nei in old_node.neighbors:
-        #             if nei not in old_to_new:
-        #                 new_node = Node(nei.val, [])
-        #                 old_to_new[nei] = new_node
-        #                 q.append(nei)
-        #             old_to_new[old_node].neighbors.append(old_to_new[nei])  
-
-        #     return old_to_new[root]
-
-        # return (bfs(node))
-
-
-                    
-
-
+        
