5fb6d729889f9b7379636bcab321ee3a093dfcd0
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-11 15:06:04 -0500

    Create file

---
+++ 1358-NumberofSubstringsContainingAllThreeCharacters/1358-NumberofSubstringsContainingAllThreeCharacters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        # find invalid
+        left = 0
+        n = len(s)
+        d = {"a":0, "b":0, "c":0}
+        def is_valid(freq_dict): 
+            for key, value in freq_dict.items():
+                if value < 1:
+                    return False
+            return True
+        result = 0
+        for right in range(n):
+            d[s[right]] += 1
+            while is_valid(d):
+                result += n - right
+                d[s[left]] -= 1
+                left += 1
+
+
+        return result
