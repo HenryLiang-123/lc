e2c2f6612e7000175e665ad19b4cfde337541682
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-12 17:24:44 -0500

    File updated at 3/12/2025, 5:24:44 PM

--- 2523-ClosestPrimeNumbersinRange/2523-ClosestPrimeNumbersinRange.py
+++ 2523-ClosestPrimeNumbersinRange/2523-ClosestPrimeNumbersinRange.py
@@ -1,28 +1,27 @@
+// Last updated: 3/12/2025, 5:24:44 PM
 import math
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
-        prime = [True for _ in range(right+1)]
-        prime[0], prime[1] = False, False
+        sieve = [True for _ in range(right+1)]
+        sieve[0] = sieve[1] = False
 
-        divisors = math.floor(math.sqrt(right))
-
-        for i in range(2, divisors + 1):
-            if prime[i]:
+        end = math.ceil(math.sqrt(right))
+        for i in range(end+1):
+            if sieve[i]:
                 for j in range(i*i, right+1, i):
-                    prime[j] = False
-        nums = []
-        for num in range(left, right+1):
-            if prime[num]:
-                nums.append(num)
-        result = float('inf')
-        ret = [-1,-1]
-        for i in range(1, len(nums)):
-            dist = nums[i] - nums[i-1]
-            if dist < result:
-                result = min(result, dist)
-                ret[0] = nums[i-1]
-                ret[1] = nums[i]
+                    sieve[j] = False
+        result = []
+        for num, is_prime in enumerate(sieve):
+            if is_prime and num >= left:
+                result.append(num)
 
-        return ret
+        if len(result) < 2:
+            return [-1, -1]
+        diff = float('inf')
+        final = []
+        for i in range(1, len(result)):
+            if result[i] - result[i-1] < diff:
+                final = [result[i-1], result[i]]
+                diff = result[i] - result[i-1]
 
-
+        return final
