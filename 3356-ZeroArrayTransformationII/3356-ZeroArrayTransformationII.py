184cde3496c1eebcfc3dfa2c4927979dcc74e922
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-13 14:28:52 -0500

    File updated at 3/13/2025, 2:28:51 PM

--- 3356-ZeroArrayTransformationII/3356-ZeroArrayTransformationII.py
+++ 3356-ZeroArrayTransformationII/3356-ZeroArrayTransformationII.py
@@ -1,37 +1,33 @@
+// Last updated: 3/13/2025, 2:28:51 PM
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        n = len(nums)
-        
-        def can_make_zero_array(k):
-            diff = [0] * (n + 1)
-            
+
+        def can_achieve(k):
+            n = len(nums)
+            diff_arr = [0 for _ in range(n)]
+
             for i in range(k):
-                left, right, val = queries[i]
-                diff[left] += val
-                diff[right + 1] -= val
-            
-            curr_val = 0
+                start, end, value = queries[i]
+                diff_arr[start] -= value
+                if end < n-1:
+                    diff_arr[end+1] += value
+            for i in range(1, n):
+                diff_arr[i] = diff_arr[i-1] + diff_arr[i]
+
             for i in range(n):
-                curr_val += diff[i]
-                if curr_val < nums[i]:
+                if diff_arr[i] + nums[i] > 0:
                     return False
-            
+
             return True
-        
-        if all(x == 0 for x in nums):
-            return 0
-        
-        left, right = 1, len(queries)
-        
-        if not can_make_zero_array(right):
-            return -1
-        
-        while left < right:
-            mid = left + (right - left) // 2
-            
-            if can_make_zero_array(mid):
-                right = mid
+
+        left = 0
+        right = len(queries)
+
+        while left <= right:
+            mid = (left + right) // 2
+            if can_achieve(mid):
+                right = mid - 1
             else:
                 left = mid + 1
-        
-        return left
+
+        return left if left <= len(queries) else -1
