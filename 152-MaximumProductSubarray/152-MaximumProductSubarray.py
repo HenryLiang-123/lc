8c40620c3936007087a43e4371940701510dda7e
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-14 16:09:01 -0500

    File updated at 3/14/2025, 4:09:01 PM

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.py
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.py
@@ -1,4 +1,4 @@
-// Last updated: 3/14/2025, 4:07:53 PM
+// Last updated: 3/14/2025, 4:09:01 PM
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
@@ -11,11 +11,12 @@ class Solution:
             return max_so_far
     
         for i in range(1, n):
+
+            result = max(nums[i], result, max_so_far * nums[i], min_so_far * nums[i])
             temp = max_so_far
             max_so_far = max(nums[i], max_so_far * nums[i], min_so_far * nums[i])
             min_so_far = min(nums[i], temp * nums[i], min_so_far * nums[i])
-
-            result = max(result, max_so_far, min_so_far)
+            
             
 
 
