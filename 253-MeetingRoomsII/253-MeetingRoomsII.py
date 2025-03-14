68d1f7f109c3d8d162f747aef1aa8aa11878e8ba
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-14 18:17:36 -0500

    Create file

---
+++ 253-MeetingRoomsII/253-MeetingRoomsII.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        starts = sorted([x[0] for x in intervals])
+        ends = sorted([x[1] for x in intervals])
+        n = len(starts)
+        rooms = 0
+        e = 0
+        for s in range(n):
+            rooms += 1
+            if starts[s] >= ends[e]:
+                rooms -= 1
+                e += 1
+
+        return rooms
+
