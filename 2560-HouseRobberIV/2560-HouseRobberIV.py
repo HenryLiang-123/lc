cee88f5d964196f1116bfff53e95bfffb56e2d93
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-15 13:55:55 -0500

    Create file

---
+++ 2560-HouseRobberIV/2560-HouseRobberIV.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        def is_valid(capability):
+            idx = []
+            for i in range(n):
+                if nums[i] < capability:
+                    if len(idx) > 0:
+                        if idx[-1] + 1 != i:
+                            idx.append(i)
+                    else:
+                        idx.append(i)
+
+            # if len(idx) == 2:
+            #     return idx[1] - idx[0] > 1
+            
+            return len(idx) >= k
+
+        left = min(nums)
+        right = max(nums)
+
+        while left <= right:
+            mid = (left + right) // 2
+            if is_valid(mid):
+                right = mid -1
+            else:
+                left = mid + 1
+
+        return left-1
+                
