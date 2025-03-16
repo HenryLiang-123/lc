46d0e0239e21c3104be58a552c45836cfb371d64
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-15 21:02:21 -0500

    File updated at 3/15/2025, 9:02:21 PM

--- 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.py
+++ 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.py
@@ -1,12 +1,12 @@
-// Last updated: 3/15/2025, 9:00:43 PM
+// Last updated: 3/15/2025, 9:02:21 PM
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        bought = -prices[0]
+        bought = float('-inf')
         sold = float('-inf')
         cooled = 0
         n = len(prices)
 
-        for i in range(1, n):
+        for i in range(n):
             temp_bought = bought
             temp_sold = sold
             
