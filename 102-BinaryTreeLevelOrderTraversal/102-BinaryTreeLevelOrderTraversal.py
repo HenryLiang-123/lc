c4dd1ad180b8ee063d693d8c3639008e8cd16c0f
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-18 14:40:24 -0500

    File updated at 3/18/2025, 2:40:24 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,3 +1,4 @@
+// Last updated: 3/18/2025, 2:40:24 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,26 +8,26 @@
 from collections import deque
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
-        result = []
-        def bfs(node):
-            q = deque([node])
+        
+        def bfs():
+            if not root:
+                return []
+            q = deque([root])
+            result = []
             while q:
-                curr_level = len(q)
-                curr_nodes = []
-                for i in range(curr_level):
-                    curr = q.popleft()
-                    if curr.left:
-                        q.append(curr.left)
-                    if curr.right:
-                        q.append(curr.right)
+                level_size = len(q)
+                level_elements = []
+                for i in range(level_size):
+                    node = q.popleft()
+                    level_elements.append(node.val)
+                    if node.left:
+                        q.append(node.left)
+                    
+                    if node.right:
+                        q.append(node.right)
 
-                    curr_nodes.append(curr.val)
+                result.append(level_elements)
+            
+            return result
 
-                result.append(curr_nodes)
-        bfs(root)
-        return result
-
-
-
+        return bfs()
