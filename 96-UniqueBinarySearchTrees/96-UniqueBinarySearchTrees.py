eb8e9fbcdd433444a948746a6fc7e397810ad5a8
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-18 14:26:20 -0500

    File updated at 3/18/2025, 2:26:20 PM

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
@@ -1,20 +1,22 @@
-// Last updated: 3/3/2025, 7:17:13 PM
+// Last updated: 3/18/2025, 2:26:20 PM
 class Solution:
     def numTrees(self, n: int) -> int:
         memo = {}
         def dfs(left, right):
+            
             if (left, right) in memo:
                 return memo[(left, right)]
 
             if left > right:
                 return 1
+            
+            result = 0
 
-            res = 0
             for i in range(left, right+1):
-                left_trees = dfs(left, i-1)
-                right_trees = dfs(i+1, right)
-                res += left_trees * right_trees
-            memo[(left, right)] = res
-            return res
+                result += dfs(left, i-1) * dfs(i+1, right)
+
+            memo[(left, right)] = result
+
+            return memo[(left, right)]
 
         return dfs(0, n-1)
