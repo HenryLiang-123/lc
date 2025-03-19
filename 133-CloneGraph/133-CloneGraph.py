250baacc2272ac88968c310bd8c3bd416963169a
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-19 14:26:52 -0500

    File updated at 3/19/2025, 2:26:52 PM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,4 +1,4 @@
-// Last updated: 3/9/2025, 10:38:32 PM
+// Last updated: 3/19/2025, 2:26:52 PM
 """
 # Definition for a Node.
 class Node:
@@ -12,19 +12,21 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         old_to_new = {}
 
-        def dfs(root):
-            if not root:
-                return root
+        def dfs(node):
+            if not node:
+                return None
 
-            if root in old_to_new:
-                return old_to_new[root]
+            if node in old_to_new:
+                return old_to_new[node]
 
-            new_node = Node(root.val, [])
-            old_to_new[root] = new_node
-            for nei in root.neighbors:
+            new_node = Node(node.val)
+            
+            old_to_new[node] = new_node
+            for nei in node.neighbors:
                 new_node.neighbors.append(dfs(nei))
+
+            
 
             return new_node
 
         return dfs(node)
-        
