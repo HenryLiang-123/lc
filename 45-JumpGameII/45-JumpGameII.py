dffa4bb544c8175beb1491b5f316c3f127b45796
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-19 14:18:59 -0500

    File updated at 3/19/2025, 2:18:58 PM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,23 +1,21 @@
-// Last updated: 3/14/2025, 5:44:03 PM
+// Last updated: 3/19/2025, 2:18:58 PM
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        levels = 0
         n = len(nums)
-        left = 0
-        right = 0
-        res = 0
+        level_start = 0
+        level_end = 0
+        furthest = 0
 
-        while right < n-1:
-            farthest = 0
-            for i in range(left, right + 1):
-                farthest = max(farthest, i + nums[i])
-
-            left = right + 1
-            right = farthest
-            res += 1
-
-        return res
+        while level_end < n-1:
+            for i in range(level_start, level_end+1):
+                furthest = max(furthest, i + nums[i])
+            level_start = level_end + 1
+            level_end = furthest
+            levels += 1
 
 
+                
 
 
-            
+        return levels
