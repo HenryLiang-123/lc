dee387d6c986976096fcd52301dad9401b572b2e
Henry Liang <62863558+HenryLiang-123@users.noreply.github.com>
2025-03-23 15:26:16 -0500

    File updated at 3/23/2025, 3:26:16 PM

--- 1976-NumberofWaystoArriveatDestination/1976-NumberofWaystoArriveatDestination.py
+++ 1976-NumberofWaystoArriveatDestination/1976-NumberofWaystoArriveatDestination.py
@@ -1,42 +1,39 @@
-# Last updated: 3/23/2025, 3:20:09 PM
+# Last updated: 3/23/2025, 3:26:16 PM
 import heapq
-from collections import defaultdict
-
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        # bfs (dijkstra) with tracking of cost to node, if less, reset count to ways to curr_node, and change cost. If equal, ways to nei = ways to nei + ways to curr_node
         MOD = 10**9 + 7
         adj_list = defaultdict(list)
         for src, dst, cost in roads:
             adj_list[src].append((dst, cost))
             adj_list[dst].append((src, cost))
 
-        # Initialize distances to infinity except for source node
         cost_to_node = {i: float('inf') for i in range(n)}
         cost_to_node[0] = 0
-        
-        ways_to_node = [0] * n
+        ways_to_node = {i: 0 for i in range(n)}
         ways_to_node[0] = 1
-        
-        # Priority queue storing (cost, node)
-        pq = [(0, 0)]  # (cost, node)
-        
-        while pq:
-            curr_cost, node = heapq.heappop(pq)
-            
-            # Skip if we've found a better path already
-            if curr_cost > cost_to_node[node]:
-                continue
-                
-            for nei, edge_cost in adj_list[node]:
-                new_cost = curr_cost + edge_cost
-                
-                # Found a shorter path
-                if new_cost < cost_to_node[nei]:
-                    cost_to_node[nei] = new_cost
-                    ways_to_node[nei] = ways_to_node[node]
-                    heapq.heappush(pq, (new_cost, nei))
-                # Found another path with the same cost
-                elif new_cost == cost_to_node[nei]:
-                    ways_to_node[nei] = (ways_to_node[nei] + ways_to_node[node]) % MOD
-        
+
+        def bfs(src):
+            q = [(0, 0)]
+            heapq.heapify(q)
+            while q:
+                cost, node = heapq.heappop(q)
+                if cost > cost_to_node[node]:
+                    continue
+                for nei, cost_to_nei in adj_list[node]:    
+
+                    if cost + cost_to_nei < cost_to_node[nei]:
+                        cost_to_node[nei] = cost + cost_to_nei
+                        heapq.heappush(q, (cost + cost_to_nei, nei))
+                        ways_to_node[nei] = ways_to_node[node]
+                        
+                    elif cost + cost_to_nei == cost_to_node[nei]:
+                        ways_to_node[nei] = (ways_to_node[node] + ways_to_node[nei]) % MOD
+                       
+
+
+        bfs(0)
         return ways_to_node[n-1]
+
+            
